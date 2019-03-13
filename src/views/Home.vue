7f07e92b99aabe5ef48489752b51df3f8f47dca1
juaki <juaki.lop@gmail.com>
2019-03-13 20:45:12 +0100

    init demo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <p>{{title}}</p>
+    <input type="text" v-on:input="changeTitle">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld,
+  name: "home",
+  data() {
+    return {
+      title: "Hola caracola"
+    };
   },
+  methods: {
+    changeTitle: function(event) {
+      console.log("Event", event);
+      this.title = event.target.value;
+    }
+  },
+  components: {
+    HelloWorld
+  }
 };
 </script>
