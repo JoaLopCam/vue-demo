32bdeeff1fd4a730749ac1fa0292f626654252ad
juaki <juaki.lop@gmail.com>
2019-03-14 01:11:34 +0100

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,9 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <p>{{title}}</p>
-    <input type="text" v-on:input="changeTitle">
+    <p>{{returnInterpolation()}} jiji</p>
+    <a v-bind:href="link">Link a google</a>
+    <input value="Escucha dos eventos" type="text" v-on="{ input: changeTitle, click: doThat }">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -15,13 +17,21 @@ export default {
   name: "home",
   data() {
     return {
-      title: "Hola caracola"
+      title: "Hola caracola",
+      careThis: "Usando this en methods",
+      link: "http://google.com"
     };
   },
   methods: {
     changeTitle: function(event) {
       console.log("Event", event);
       this.title = event.target.value;
+    },
+    doThat: function(event) {
+      console.log("Click", event);
+    },
+    returnInterpolation: function(event) {
+      return this.careThis;
     }
   },
   components: {
